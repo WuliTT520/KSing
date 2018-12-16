472423c09395dcd16a20f121e98e61b760b5d85f
flaghb <45229286+flaghb@users.noreply.github.com>
2018-12-16 20:47:38 +0800

    修复评论闪退

--- app/src/main/java/com/homework/ksing/ksing/ui/EvaluateActivity.java
+++ app/src/main/java/com/homework/ksing/ksing/ui/EvaluateActivity.java
@@ -73,22 +73,72 @@ public class EvaluateActivity extends AppCompatActivity implements View.OnClickL
     private String song_picture;
     private SharedPreferences sp;
     private MyURL myURL=new MyURL();
-    private String testJson = "{\n" +
+    private String testJson="";
+    private String testJson1="{\n" +
             "\t\"code\": 1000,\n" +
             "\t\"message\": \"查看评论成功\",\n" +
             "\t\"data\": {\n" +
             "\t\t\"total\": 3,\n" +
-            "\t\t\"list\": [{\n" ;
-
+            "\t\t\"list\": [{\n" +
+            "\t\t\t\t\"id\": 42,\n" +
+            "\t\t\t\t\"nickName\": \"程序猿\",\n" +
+            "\t\t\t\t\"userLogo\": \"http://ucardstorevideo.b0.upaiyun.com/userLogo/9fa13ec6-dddd-46cb-9df0-4bbb32d83fc1.png\",\n" +
+            "\t\t\t\t\"content\": \"时间是一切财富中最宝贵的财富。\",\n" +
+            "\t\t\t\t\"imgId\": \"xcclsscrt0tev11ok364\",\n" +
+            "\t\t\t\t\"replyTotal\": 1,\n" +
+            "\t\t\t\t\"createDate\": \"三分钟前\",\n" +
+            "\t\t\t\t\"replyList\": [{\n" +
+            "\t\t\t\t\t\"nickName\": \"沐風\",\n" +
+            "\t\t\t\t\t\"userLogo\": \"http://ucardstorevideo.b0.upaiyun.com/userLogo/9fa13ec6-dddd-46cb-9df0-4bbb32d83fc1.png\",\n" +
+            "\t\t\t\t\t\"id\": 40,\n" +
+            "\t\t\t\t\t\"commentId\": \"42\",\n" +
+            "\t\t\t\t\t\"content\": \"时间总是在不经意中擦肩而过,不留一点痕迹.\",\n" +
+            "\t\t\t\t\t\"status\": \"01\",\n" +
+            "\t\t\t\t\t\"createDate\": \"一个小时前\"\n" +
+            "\t\t\t\t}]\n" +
+            "\t\t\t},\n" +
+            "\t\t\t{\n" +
+            "\t\t\t\t\"id\": 41,\n" +
+            "\t\t\t\t\"nickName\": \"设计狗\",\n" +
+            "\t\t\t\t\"userLogo\": \"http://ucardstorevideo.b0.upaiyun.com/userLogo/9fa13ec6-dddd-46cb-9df0-4bbb32d83fc1.png\",\n" +
+            "\t\t\t\t\"content\": \"这世界要是没有爱情，它在我们心中还会有什么意义！这就如一盏没有亮光的走马灯。\",\n" +
+            "\t\t\t\t\"imgId\": \"xcclsscrt0tev11ok364\",\n" +
+            "\t\t\t\t\"replyTotal\": 1,\n" +
+            "\t\t\t\t\"createDate\": \"一天前\",\n" +
+            "\t\t\t\t\"replyList\": [{\n" +
+            "\t\t\t\t\t\"nickName\": \"沐風\",\n" +
+            "\t\t\t\t\t\"userLogo\": \"http://ucardstorevideo.b0.upaiyun.com/userLogo/9fa13ec6-dddd-46cb-9df0-4bbb32d83fc1.png\",\n" +
+            "\t\t\t\t\t\"commentId\": \"41\",\n" +
+            "\t\t\t\t\t\"content\": \"时间总是在不经意中擦肩而过,不留一点痕迹.\",\n" +
+            "\t\t\t\t\t\"status\": \"01\",\n" +
+            "\t\t\t\t\t\"createDate\": \"三小时前\"\n" +
+            "\t\t\t\t}]\n" +
+            "\t\t\t},\n" +
+            "\t\t\t{\n" +
+            "\t\t\t\t\"id\": 40,\n" +
+            "\t\t\t\t\"nickName\": \"产品喵\",\n" +
+            "\t\t\t\t\"userLogo\": \"http://ucardstorevideo.b0.upaiyun.com/userLogo/9fa13ec6-dddd-46cb-9df0-4bbb32d83fc1.png\",\n" +
+            "\t\t\t\t\"content\": \"笨蛋自以为聪明，聪明人才知道自己是笨蛋。\",\n" +
+            "\t\t\t\t\"imgId\": \"xcclsscrt0tev11ok364\",\n" +
+            "\t\t\t\t\"replyTotal\": 0,\n" +
+            "\t\t\t\t\"createDate\": \"三天前\",\n" +
+            "\t\t\t\t\"replyList\": []\n" +
+            "\t\t\t}\n" +
+            "\t\t]\n" +
+            "\t}\n" +
+            "}";
     private TextView myName;
     private TextView myText;
     private TextView songName;
     private CircleImageView myDp;
-    @Override
+
+
+            @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_evaluate);
+
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         System.out.println(testJson);
         initView();
@@ -114,8 +164,7 @@ public class EvaluateActivity extends AppCompatActivity implements View.OnClickL
         CollapsingToolbarLayout collapsingToolbar =
                 (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setTitle("详情");
-        commentsList = generateTestData();
-        initExpandableListView(commentsList);
+
         songbg.setImageURL(myURL.getURL()+song_picture);
         songName.setText(intent.getExtras().getString("songName"));
 
@@ -130,9 +179,11 @@ public class EvaluateActivity extends AppCompatActivity implements View.OnClickL
         //默认展开所有回复
         adapter = new CommentExpandAdapter(this, commentList);
         expandableListView.setAdapter(adapter);
-        for(int i = 0; i<commentList.size(); i++){
+
+        for (int i = 0; i < commentList.size(); i++) {
             expandableListView.expandGroup(i);
         }
+
         expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
             @Override
             public boolean onGroupClick(ExpandableListView expandableListView, View view, int groupPosition, long l) {
@@ -175,6 +226,7 @@ public class EvaluateActivity extends AppCompatActivity implements View.OnClickL
         Gson gson = new Gson();
         commentBean = gson.fromJson(testJson, CommentBean.class);
         List<CommentDetailBean> commentList = commentBean.getData().getList();
+
         return commentList;
     }
 
@@ -347,7 +399,12 @@ public class EvaluateActivity extends AppCompatActivity implements View.OnClickL
                     data = new JSONObject(result);
                     myName.setText(data.getString("name"));
                     myText.setText(data.getString("user_text"));
-
+                    testJson+= "{\n" +
+                            "\t\"code\": 1000,\n" +
+                            "\t\"message\": \"查看评论成功\",\n" +
+                            "\t\"data\": {\n" +
+                            "\t\t\"total\": 3,\n" +
+                            "\t\t\"list\": [{\n" ;
                     JSONArray dateArray=data.getJSONArray("comments");
                     for(int i=0;i<dateArray.length();i++)
                     {
@@ -383,6 +440,8 @@ public class EvaluateActivity extends AppCompatActivity implements View.OnClickL
 
 
                     }
+                    commentsList = generateTestData();
+                    initExpandableListView(commentsList);
 
                     System.out.println(testJson);
 
