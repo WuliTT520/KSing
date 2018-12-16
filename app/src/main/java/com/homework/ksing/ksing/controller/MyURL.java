06a491f2694668e92021f9fe81a7b98bf2031c2a
WuliTT520 <mlh32356516535>
2018-12-17 00:07:53 +0800

    简单界面
    This reverts commit f120e20d2403b206ceb7e0bc888028631f4c96f1.

--- app/src/main/java/com/homework/ksing/ksing/controller/MyURL.java
+++ app/src/main/java/com/homework/ksing/ksing/controller/MyURL.java
@@ -3,7 +3,7 @@ package com.homework.ksing.ksing.controller;
 
 
 public class MyURL {
-    private String URL="http://10.11.186.14:8080";
+    private String URL="http://10.11.181.29:8080";
 
     public String getURL() {
         return URL;
