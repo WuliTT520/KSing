2f3829d3425da3885e1d0b57f7a17acbe4d0f301
flaghb <45229286+flaghb@users.noreply.github.com>
2018-12-18 16:56:13 +0800

    上传

--- app/src/main/java/com/homework/ksing/ksing/ui/UpfileAndDynamicActivity.java
+++ app/src/main/java/com/homework/ksing/ksing/ui/UpfileAndDynamicActivity.java
@@ -1,20 +1,40 @@
 package com.homework.ksing.ksing.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.homework.ksing.ksing.R;
+import com.homework.ksing.ksing.controller.MyURL;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class UpfileAndDynamicActivity extends AppCompatActivity {
     private Button bofang;
@@ -22,6 +42,10 @@ public class UpfileAndDynamicActivity extends AppCompatActivity {
     private String filePath;
     private MediaPlayer mediaPlayer;
     private ImageView back;
+    private File audioFile;
+    private SharedPreferences sp;
+    private TextView upload;
+    private MyURL myURL=new MyURL();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,8 +68,13 @@ public class UpfileAndDynamicActivity extends AppCompatActivity {
         bofang=findViewById(R.id.bofang);
         back=findViewById(R.id.uback);
         stop=findViewById(R.id.stop);
+        upload=findViewById(R.id.upload);
+
+        sp = this.getSharedPreferences("userInfo", Context.MODE_PRIVATE);
         Intent intent = getIntent();
         filePath=intent.getExtras().getString("filePath");
+        audioFile=new File(filePath);
+
 
     }
     public void setListener() {
@@ -79,10 +108,57 @@ public class UpfileAndDynamicActivity extends AppCompatActivity {
                 mediaPlayer.stop();
             }
         });
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getInChildThread(myURL.getURL()+"/uploadMySong",audioFile);
+            }
+        });
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+    }
+
+    private void getInChildThread(String url,File file) {
+
+        sp = this.getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .readTimeout(5, TimeUnit.SECONDS)
+                .build();
+
+        MultipartBody.Builder builder = new MultipartBody.Builder().setType(MultipartBody.FORM);
+        builder.addFormDataPart( //给Builder添加上传的文件
+                "upload",  //请求的名字
+                file.getName(), //文件的文字，服务器端用来解析的
+                RequestBody.create(MediaType.parse(".wav"), file));
+
+
+
+        //post请求来获得数据
+        //创建一个RequestBody，存放重要数据的键值对
+
+        //创建一个请求对象，传入URL地址和相关数据的键值对的对象
+
+        Request.Builder builder1 = new Request.Builder();
+        builder1.url(url)
+                .post(builder.build());
+        //创建一个能处理请求数据的操作类
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Call call = okHttpClient.newCall(builder1.build());
+
+        //使用异步任务的模式请求数据
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("TAG", "错误信息：" + e.toString());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
             }
         });
     }
